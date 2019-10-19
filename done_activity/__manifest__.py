27f6d9eb656a5080ed8ea6064c8d6728a462330f
Ziad799 <56259761+Ziad799@users.noreply.github.com>
2019-10-19 02:41:51 -0700

    Add files via upload

---
+++ done_activity/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Show Done Activities',
+    'summary': 'Show Done Activity details in the crm module',
+    'Author': 'Ziad Monim',
+    'description': """
+Show Done Activity
+===================
+
+Show Done Activity details in the crm modules
+
+after install the module go to CRM/Reporting/Done Activites
+    """,
+    'depends': ['crm','sales_team'],
+    'data': [
+        'views/done_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
